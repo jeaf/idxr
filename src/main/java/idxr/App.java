ddd377af19f07abe5766021cae6f3cecc2c7d177
Francois Jeannotte <francois.jeannotte@gmail.com>
2018-12-11 10:41:01 -0500

    Start work on DB schema, some cleanup

--- src/main/java/idxr/App.java
+++ src/main/java/idxr/App.java
@@ -2,33 +2,43 @@ package idxr;
 
 import com.beust.jcommander.JCommander;
 import java.sql.Connection;
+import java.util.logging.Logger;
 
 public class App
 {
     public static void main(String[] argv)
     {
-        CmdLineArgs args = new CmdLineArgs();
-        JCommander jc = new JCommander(args);
-        jc.setProgramName("idxr");
-        jc.parse(argv);
-
-        if (args.help)
+        try
         {
-            jc.usage();
-            return;
+            CmdLineArgs args = new CmdLineArgs();
+            JCommander jc = new JCommander(args);
+            jc.setProgramName("idxr");
+            jc.parse(argv);
+
+            if (args.help)
+            {
+                jc.usage();
+                return;
+            }
+
+            switch (args.cmd)
+            {
+            case "parse":
+                log.info(String.format("Executing parse, db: %s", args.dbPath));
+                Db db = new Db("abc");
+                Connection c = db.connect();
+                break;
+            default:
+                System.out.printf("Invalid command: %s\n", args.cmd);
+                break;
+            }
         }
-
-        switch (args.cmd)
+        catch (Exception ex)
         {
-        case "parse":
-            System.out.printf("Executing parse, db: %s\n", args.dbPath);
-            Db db = new Db("abc");
-            Connection c = db.connect();
-            break;
-        default:
-            System.out.printf("Invalid command: %s\n", args.cmd);
-            break;
+            log.severe(ex.getMessage());
         }
     }
+
+    private final static Logger log = Logger.getLogger(Db.class.getName());
 }
 
--- src/main/java/idxr/Db.java
+++ src/main/java/idxr/Db.java
@@ -1,6 +1,7 @@
 package idxr;
 
 import java.sql.*;
+import java.util.logging.Logger;
 
 public class Db
 {
@@ -9,42 +10,79 @@ public class Db
         path = p;
     }
 
-    public Connection connect()
+    public Connection connect() throws SQLException
     {
-        Connection conn = null;
-        try
+        String url = String.format("jdbc:sqlite:%s", path);
+        try (Connection conn = DriverManager.getConnection(url))
         {
-            // db parameters
-            //String url = "jdbc:sqlite:C:/sqlite/db/chinook.db";
-            String url = "jdbc:sqlite::memory:";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-            
-            System.out.println("Connection to SQLite has been established.");
-            
+            log.info("Connection to SQLite has been established.");
+            initTables(conn);
+            return conn;
         }
-        catch (SQLException e)
-        {
-            System.out.println(e.getMessage());
-        }
-        finally
-        {
-            try
-            {
-                if (conn != null)
-                {
-                    conn.close();
-                }
-            }
-            catch (SQLException ex)
-            {
-                System.out.println(ex.getMessage());
-            }
-        }
+        //catch (SQLException e)
+        //{
+        //    log.severe(e.getMessage());
+        //}
+        //finally
+        //{
+        //    try
+        //    {
+        //        if (conn != null) conn.close();
+        //        conn = null;
+        //    }
+        //    catch (SQLException ex)
+        //    {
+        //        log.severe(ex.getMessage());
+        //    }
+        //}
 
-        return conn;
+        //return conn;
+    }
+
+    private void initTables(Connection c) throws SQLException
+    {
+        log.info("Initializing DB tables");
+        String sql;
+        sql = "CREATE TABLE IF NOT EXISTS doc(" +
+               "id           INTEGER PRIMARY KEY," +
+               "type         INTEGER NOT NULL," + // todo: add check constraint, 1-file, 2-webpage, 3-email, etc.
+               "path         TEXT NOT NULL UNIQUE," +
+               "ctime        INTEGER NOT NULL," +
+               "ctime_db     INTEGER NOT NULL DEFAULT (strftime('%s', 'now'))," + // todo: add trigger
+               "mtime        INTEGER NOT NULL," +
+               "mtime_db     INTEGER NOT NULL DEFAULT (strftime('%s', 'now'));" +
+               "md5          BLOB NOT NULL," +
+               "size         INTEGER NOT NULL," +
+               "mime_type    TEXT," +
+               "mime_subtype TEXT," +
+               "width        INTEGER," +
+               "height       INTEGER," +
+               "duration     INTEGER," +
+               "date_taken   INTEGER);" +
+               "CREATE TABLE IF NOT EXISTS color_moments(" +
+               "id           INTEGER PRIMARY KEY," +
+               "doc_id       INTEGER NOT NULL REFERENCES doc(id)," +
+               "frame_num    INTEGER NOT NULL DEFAULT 0," +
+               "mean_h       REAL," +
+               "mean_s       REAL," +
+               "mean_v       REAL," +
+               "stddev_h     REAL," +
+               "stddev_s     REAL," +
+               "stddev_v     REAL," +
+               "skew_h       REAL," +
+               "skew_s       REAL," +
+               "skew_v       REAL," +
+               "UNIQUE(doc_id, frame_num));" +
+               "CREATE TABLE IF NOT EXISTS match(" +
+               "word   TEXT NOT NULL," +
+               "doc_id INTEGER NOT NULL REFERENCES doc(id)," +
+               "flags  INTEGER NOT NULL DEFAULT 0," +
+               "score  INTEGER NOT NULL DEFAULT 0);";
+        Statement stmt = c.createStatement();
+        stmt.execute(sql);
     }
 
     private String path;
+    private final static Logger log = Logger.getLogger(Db.class.getName());
 }
 
